2da50c8d8e90388e478b198097fefb6093ca2749
Javier <profesor.jjaviermc@ies-azarquiel.es>
2023-12-21 08:54:34 +0100

    21/12/2023

--- arrays/src/pruebaArrayList/Temperaturas.java
+++ arrays/src/pruebaArrayList/Temperaturas.java
@@ -11,6 +11,14 @@ public class Temperaturas {
 		ArrayList<Double> temperaturas = new ArrayList<Double>(Arrays.asList(8.4, 10.3, 12.5, 11.0, 11.3, 10.6, 9.9)); 
 
 		//Sacar la media de las temperaturas
+		double media, suma = 0;
+		for (Double double1 : temperaturas) {
+			suma += double1;
+		}
+		media = suma / temperaturas.size();
+		System.out.println("Temperatura media: " + media);
+		
+		//temperatura máxima
 		
 		//¿Que dia fué el mas caluroso?
 		
